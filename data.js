7999c76c858ffd3c83a788bb2e0efca515ab58cd
Sunil <sunilchaudhary7789@gmail.com>
2024-10-27 00:22:12 +0530

    updated the ui

--- data.js
+++ data.js
@@ -3,25 +3,141 @@ export const speakers = [
         name: "John Doe",
         imageUrl: 'https://s3-alpha-sig.figma.com/img/d6a6/fffd/2100d987b5946aa1df33cf0f6c34a418?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LOm6dnYet6jpAKmbCYFb74vdC~rtxQmFdgYhDSmt71gr1~qo7MF4EGcjdHi8aOxRc4qu1GW5X4w1G7MTdOyXx76TjqEZbKLOchyCPlU0QlRVEdZfR9jn9mLR2OXDPdP6vMqBcQ0surXNA0okBmrNFPJ-QpkY5avXUulsg0thv7X4i3iVkktQ65vGc2g9bh6MFDdwO4u~FW4x4YuAQJ28Ght6ksLYtb4rHciYFxyxpKfkcBQdWnHGrCcDfAhw3wt2DvpZ2SR6pYRcHH41uy76cUc6GMsiBI2olZbCcvmj9FCMjHHhcSrxhHbitVphHkg4Sh6~qRGnkrx23jCi8~GIrg__',
         role: 'Chief Marketing Officer',
-        company: 'Acme Corp'
+        company: 'Acme Corp',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
     },
     {
-        name: "John Doe",
+        name: "John Doe2",
         imageUrl: 'https://s3-alpha-sig.figma.com/img/9de3/b8aa/4e6fd86d00bc645311899b3fbcca52d5?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VSXLLmE1VcLypFVxurLVb7sF9R1P5xlxF27ODBCH3f0HlxzlLPQbfvSXEUOlhDCQEU~4AjRP5X~U1OD72zzlvoX3JgVPn3bDgnszGqmzXAIaYxhE~woBXvOPOSBNXxfyeNOaLkq4sMTD8AQwijfaGJVRIKxyimGVzOZLKuQ-gnvSiRkHm-voKTy-JDP5HmdXpTVFrEM-bX-Gihvb44mCF5FzfEcIJUzWiAZnXQeJ9yef5qbyzhhXDKpdTROSBR7fmDElCBx0D~MAa~bsbvi58Oaj8sJONL7lbmBfEdLm1RptHlxKdV6qgJRfWR2l00r3Xlh2bMj5QWjjSEpJ4CSA1A__',
         role: 'Chief Engagement Officer',
-        company: 'Acquia'
+        company: 'Acquia',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
     },
 
     {
-        name: 'John Doe',
+        name: 'John Doe3',
         imageUrl: 'https://s3-alpha-sig.figma.com/img/9134/0d19/81845f21043dd8f5192da248767c4eae?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BFlex98RhfTfgiYKlDcXuhg5XpicwdWIi-uusX3NQp2jTSHmhC71X6XHg1MBunX08Pcv7DE3cgbPbTXldT1sSoKKy3MYvAJrIMY1yK8ZSnDfyFIkWmsUNY4qhYb9KCnv6tFICUWbt3B9Da9o9pU7pxcWXE8QhY5Y6SgVB3rPcLm9JGBMueS6NQkyFC0AZUte3Akej0nPQ3yZdh-pC80E7yvogcU4Mw8EmQzjAh6ETmMR3AQdiCepFVfpHLKXaAdjU4F3u~b9JNzeYf81UtKX2bx0cjJXilLbLrDKEZ4sVS7Ef61l-MxAKTPVLVlT0hWqjHSbEXKTAKizBtIy96wmXw__',
         role: 'Chief Technical Officer',
-        company: 'Pantheon'
+        company: 'Pantheon',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
     },
     {
-        name: "John Doe",
+        name: "John Doe4",
         imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
         role: 'Chief Marketing Officer',
-        company: 'Specbee'
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe5",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe6",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe7",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe8",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe9",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe10",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe11",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe12",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe13",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
+    },
+    {
+        name: "John Doe14",
+        imageUrl: 'https://s3-alpha-sig.figma.com/img/aeba/3e61/75255eef4aafbeaf378e32d61894f628?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jXOCEgFA~8SLXFB1ybcFCNkmtKtxv29YyJpYuikZOaxpEh~arr6JAm4mlBCiChD-L~tcSfXCTDqcYrzWv8PuDtBDKp~AgEF1439Gk--ZrHzY2RIqfluwUHhwg2f-PYfBARS6IwPRXw4jiNZyWs2z2cG~g9M9K06BtPsC0KpAuDNQunfLJoZz0ezWLwCYeW5BQ9XKDXS7ylQ8lVa3i2Frh2E9zwaoZBzsTOXTPzCa2dnKNcs9xMHYcX8DGZyPhno6AOKTybup22sCW4a5OsfMGtSsfQJqtVfZMz3oP~qC0Ptv0r9jrNswxfUc7jFCVfUJuUws9U9rKG4BhxkAiWnFiw__',
+        role: 'Chief Marketing Officer',
+        company: 'Specbee',
+        summary:`Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash
+            cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel
+            infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic
+            theme areas`
     }
 ]
--- index.js
+++ index.js
@@ -1,27 +1,122 @@
 import { speakers } from "./data.js";
 
 const speakers_container = document.querySelector('.speakers-container');
+const speakerDetails_container = document.querySelector('.speaker-details');
 
-const displaySpeakers = (speakers)=>{
-    speakers.forEach((speaker)=>{
-        let figure = document.createElement('figure');
-        figure.classList= 'speakers-container__figure'
-        let img = document.createElement('img');
-        img.src=speaker.imageUrl
-        let figcaption = document.createElement('figcaption');
-        figcaption.classList = ''
-        let h2 = document.createElement('h2');
-        h2.textContent = speaker.name
-        let h3 = document.createElement('h3');
-        h3.textContent = speaker.role
-        let p = document.createElement('p');
-        p.textContent = speaker.company
-        figcaption.append(h2,h3,p);
-        figure.append(img,figcaption)
+const displaySpeakerDetails = (speaker) => {
+    speakerDetails_container.innerHTML = null;
+    speakerDetails_container.style.boxShadow = "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
+    console.log(speaker)
+    let figure = document.createElement('figure');
+    figure.classList = 'speaker-details__figure'
+    let img = document.createElement('img');
+    img.src = speaker.imageUrl
+    let figcaption = document.createElement('figcaption');
+    let h2 = document.createElement('h2');
+    h2.textContent = speaker.name
+    let h3 = document.createElement('h3');
+    h3.textContent = speaker.role
+    let p = document.createElement('p');
+    p.textContent = speaker.company
+    figcaption.append(h2, h3, p);
+    figure.append(img, figcaption)
+    let div = document.createElement('div');
+    div.classList = 'speaker-details__seprator';
+    let article = document.createElement('article');
+    article.textContent = speaker.summary
 
-        speakers_container.append(figure);
+    const SVG_NS = "http://www.w3.org/2000/svg";
+    const svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttribute("fill", "#000000");
+    svg.setAttribute("height", "9px");
+    svg.setAttribute("width", "9px");
+    svg.setAttribute("viewBox", "-78.4 -78.4 646.80 646.80");
+    svg.setAttribute("xmlns", SVG_NS);
+    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
+    svg.setAttribute("stroke", "#000000");
+    svg.setAttribute("stroke-width", "0.0049");
+
+    const polygon = document.createElementNS(SVG_NS, "polygon");
+    polygon.setAttribute("points", "456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337");
+
+    svg.appendChild(polygon);
+    svg.addEventListener('click', () => {
+        speakerDetails_container.classList = 'speaker-details--remove'
+    })
+    console.log(svg);
+    speakerDetails_container.append(figure, div, article, svg);
+
+
+}
+
+const displaySpeakers = (speakers) => {
+    let leftArrow = document.createElement('div');
+    let RightArrow = document.createElement('div');
+
+    leftArrow.textContent = '<';
+    RightArrow.textContent = '>'
+    leftArrow.classList = 'left-arrow'
+    RightArrow.classList = 'right-arrow'
+    let i =0
+   
+
+    
+
+    const speakersContainer = document.querySelector('.speakers-container-parent');
+    speakersContainer.append(leftArrow, RightArrow);
+
+    const updateSpeakers = () =>{
+        speakersContainer.querySelectorAll('.speakers-container__figure').forEach(figure => figure.remove());
+        speakers.slice(i,i+4).forEach((speaker) => {
+            let figure = document.createElement('figure');
+            figure.addEventListener('click', () => {
+                displaySpeakerDetails(speaker)
+                speakerDetails_container.classList = 'speaker-details'
+                console.log(speakerDetails_container)
+            })
+            figure.classList = 'speakers-container__figure'
+            let img = document.createElement('img');
+            img.src = speaker.imageUrl
+            let figcaption = document.createElement('figcaption');
+            let h2 = document.createElement('h2');
+            h2.textContent = speaker.name
+            let h3 = document.createElement('h3');
+            h3.textContent = speaker.role
+            let p = document.createElement('p');
+            p.textContent = speaker.company
+            figcaption.append(h2, h3, p);
+            figure.append(img, figcaption)
+    
+            speakers_container.append(figure);
+    
+        })
+
+    }
+
+    updateSpeakers();
+
+
+    RightArrow.addEventListener('click',()=>{
+       if(i+4<speakers.length){
+        i+=4;
+        updateSpeakers();
+       }else{
+        // i=0
+       }
+       console.log(i);
         
     })
+
+    leftArrow.addEventListener('click',()=>{
+        if(i>0){
+            i-=4;
+          updateSpeakers()
+        }else{
+            // i =speakers.length-1
+        }
+        console.log(i);
+    })
+
 
 }
 
